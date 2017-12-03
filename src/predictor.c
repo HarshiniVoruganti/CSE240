11a62baa4f7d41de13fd14abd235e0abc18c751f
HarshiniVoruganti <nvorugan@ucsd.edu>
2017-12-02 17:12:15 -0800

    Buggy code for Gshare

--- src/predictor.c
+++ src/predictor.c
@@ -49,10 +49,30 @@ uint8_t my_local_prediction;
 uint8_t my_prediction;
 uint32_t Local_History_Table_Index;
 int count =0;
+uint32_t temp;
 //
 //TODO: Add your own Branch Predictor data structures here
 //
 
+void update_gshare (uint32_t pc, uint8_t outcome)
+{
+      temp = (pc ^ global_history)&((int)(pow(2,ghistoryBits))-1);
+
+      if (outcome == 0)
+      {
+        if (BHT[temp] != 0)
+          BHT[temp] = BHT[temp] -1;
+          }
+        
+      if (outcome == 1)
+         {
+          if (BHT[temp]!= 3 )
+            BHT[temp] = BHT[temp] +1;
+            }
+      global_history = (global_history << 1);  
+      global_history= global_history | outcome; 
+
+}
 uint8_t global_predictor (uint32_t global_history)
 {
   uint32_t temp_index;
@@ -71,6 +91,9 @@ uint8_t local_predictor (uint32_t pc)
 else 
   return 1;
 }
+
+
+
 void init_tournament()
 {
     global_pred = malloc((int)(pow(2,ghistoryBits))*sizeof(uint8_t));
@@ -83,107 +106,8 @@ void init_tournament()
     memset(choice_pred,2,sizeof(choice_pred));
 }
 
-int i;
-void
-init_predictor()
+void update_tournament(uint32_t pc, uint8_t outcome)
 {
-  //
-  //TODO: Initialize Branch Predictor Data Structures
-  //
-  if (bpType == GSHARE)
-  {
-    global_history=0;
-    //int temp =(int)(pow(2,ghistoryBits));
-    //printf("%d\n",temp);
-    BHT = malloc((int)(pow(2,ghistoryBits))*sizeof(uint8_t)); // 0 - Strong NT; 1 - Weak NT; 2 - Weak Taken; 3 - Strong Taken
-    memset(BHT,1,sizeof(BHT));
-    //for(i=0;i<temp;i++) // Check to see if all the elements of BHT are properly being set
-    //printf("%d\n",BHT[i]);
-  } 
-  if (bpType == TOURNAMENT)
-  {
-   global_history=0;
-   init_tournament();
-  }
-}
-
-// Make a prediction for conditional branch instruction at PC 'pc'
-// Returning TAKEN indicates a prediction of taken; returning NOTTAKEN
-// indicates a prediction of not taken
-//
-uint8_t
-make_prediction(uint32_t pc)
-{
-  //
-  //TODO: Implement prediction scheme
-  //
-  
-  // Make a prediction based on the bpType
-  switch (bpType) {
-    case STATIC:
-      return TAKEN;
-    case GSHARE:
-      Gshare_index= (pc ^ global_history)&((int)(pow(2,ghistoryBits)) -1);
-      pred = BHT[Gshare_index];
-      if (pred == 0 || pred ==1 )
-        return NOTTAKEN;
-      else
-        return TAKEN;        
-    case TOURNAMENT:
-      my_global_prediction = global_predictor(global_history);
-      my_local_prediction = local_predictor(pc);
-      my_choice_prediction = choice_pred[global_history&((int)(pow(2,ghistoryBits)) -1)];
-      //if (count <5) {printf("global = %d local = %d choice = %d \n ",my_global_prediction,my_local_prediction,my_choice_prediction); count++;}
-      if ((my_choice_prediction == 0) || (my_choice_prediction ==1))
-        my_prediction = my_local_prediction;
-      else
-        my_prediction = my_global_prediction;
-      //printf("my_prediction: %d\n", my_prediction );
-      return my_prediction;
-    case CUSTOM:
-    default:
-      break;
-  }
-
-  // If there is not a compatable bpType then return NOTTAKEN
-  return NOTTAKEN;
-}
-
-// Train the predictor the last executed branch at PC 'pc' and with
-// outcome 'outcome' (true indicates that the branch was taken, false
-// indicates that the branch was not taken)
-//
-uint32_t temp;
-void
-train_predictor(uint32_t pc, uint8_t outcome)
-{
-  //
-  //TODO: Implement Predictor training
-  //
-  //printf("%d\n",outcome);
-  switch (bpType) {
-
-
-    case GSHARE:
-      temp = (pc ^ global_history)&((int)(pow(2,ghistoryBits))-1);
-
-      if (outcome == 0)
-      {
-        if (BHT[temp] != 0)
-          BHT[temp] = BHT[temp] -1;
-          }
-        
-      if (outcome == 1)
-         {
-          if (BHT[temp]!= 3 )
-            BHT[temp] = BHT[temp] +1;
-            }
-      global_history = (global_history << 1);  
-      global_history= global_history | outcome;
-
-
-    case TOURNAMENT:
-      
       //Updating my Choice Predictor
       if (my_global_prediction != my_local_prediction)
       {
@@ -234,6 +158,110 @@ train_predictor(uint32_t pc, uint8_t outcome)
       //Updating my global history
       global_history = (global_history << 1)&((int)(pow(2,ghistoryBits))-1) ;  
       global_history= global_history | outcome;
+      
+}
+
+
+void
+init_predictor()
+{
+  //
+  //TODO: Initialize Branch Predictor Data Structures
+  //
+  if (bpType == GSHARE)
+  {
+    global_history=0;
+    BHT = malloc((int)(pow(2,ghistoryBits))*sizeof(uint8_t)); // 0 - Strong NT; 1 - Weak NT; 2 - Weak Taken; 3 - Strong Taken
+    memset(BHT,1,sizeof(BHT));
+  } 
+
+  if (bpType == TOURNAMENT)
+  {
+   global_history=0;
+   init_tournament();
+  }
+
+}
+
+// Make a prediction for conditional branch instruction at PC 'pc'
+// Returning TAKEN indicates a prediction of taken; returning NOTTAKEN
+// indicates a prediction of not taken
+//
+uint8_t
+make_prediction(uint32_t pc)
+{
+  //
+  //TODO: Implement prediction scheme
+  //
+  
+  // Make a prediction based on the bpType
+  switch (bpType) {
+    case STATIC:
+      return TAKEN;
+    case GSHARE:
+      Gshare_index= (pc ^ global_history)&((int)(pow(2,ghistoryBits)) -1);
+      pred = BHT[Gshare_index];
+      if (pred == 0 || pred ==1 )
+        return NOTTAKEN;
+      else
+        return TAKEN;        
+    case TOURNAMENT:
+      my_global_prediction = global_predictor(global_history);
+      my_local_prediction = local_predictor(pc);
+      my_choice_prediction = choice_pred[global_history&((int)(pow(2,ghistoryBits)) -1)];
+      //if (count <5) {printf("global = %d local = %d choice = %d \n ",my_global_prediction,my_local_prediction,my_choice_prediction); count++;}
+      if ((my_choice_prediction == 0) || (my_choice_prediction ==1))
+        my_prediction = my_local_prediction;
+      else
+        my_prediction = my_global_prediction;
+      //printf("my_prediction: %d\n", my_prediction );
+      return my_prediction;
+    case CUSTOM:
+    default:
+      break;
+  }
+
+  // If there is not a compatable bpType then return NOTTAKEN
+  return NOTTAKEN;
+}
+
+// Train the predictor the last executed branch at PC 'pc' and with
+// outcome 'outcome' (true indicates that the branch was taken, false
+// indicates that the branch was not taken)
+//
+
+void
+train_predictor(uint32_t pc, uint8_t outcome)
+{
+  //
+  //TODO: Implement Predictor training
+  //
+  //printf("%d\n",outcome);
+  switch (bpType) {
+
+
+    case GSHARE:
+    //update_gshare(pc, outcome);
+      temp = (pc ^ global_history)&((int)(pow(2,ghistoryBits))-1);
+
+      if (outcome == 0)
+      {
+        if (BHT[temp] != 0)
+          BHT[temp] = BHT[temp] -1;
+          }
+        
+      if (outcome == 1)
+         {
+          if (BHT[temp]!= 3 )
+            BHT[temp] = BHT[temp] +1;
+            }
+      global_history = (global_history << 1);  
+      global_history= global_history | outcome;
+
+
+    case TOURNAMENT:
+
+      update_tournament(pc,outcome);      
       
     default:
       break;
